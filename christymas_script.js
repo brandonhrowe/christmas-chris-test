99d497632550ba39aaf8df588e7e29a6d3a84130
Brandon Rowe <brandonhrowe@gmail.com>
2019-01-03 22:03:45 -0500

    initial commit

---
+++ christymas_script.js
@@ -0,0 +1,7 @@
+let name = input("Here's a holiday present for Chris Davis!\nPlease enter your first name, Chris Davis: ");
+if (name==="Chris") {
+	console.log("Merry " + name.upper() + "tmas!!!");
+}
+else {
+	print("I'm sorry, this present is for Chris Davis. \nDon't steal Chris' gift, " + name + "!");
+}
